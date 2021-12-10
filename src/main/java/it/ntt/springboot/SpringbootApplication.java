c0329e1812cf2f590a4b58013f04601c6a2eeccf
AurelioPa <aurelio.paolillo@gmail.com>
2021-12-10 15:10:41 +0100

    first commit

---
+++ src/main/java/it/ntt/springboot/SpringbootApplication.java
@@ -0,0 +1,13 @@
+package it.ntt.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
